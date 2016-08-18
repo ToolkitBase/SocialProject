e5d7c778b3a15415147bafa97fcd416b0b6b620a
ToolKitBase <markswong8@gmail.com>
2016-08-17 23:33:24 -0700

    Completed setting up Firebase integration

---
+++ DerivedData/SocialProject/Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-SocialProject.build/DerivedSources/Pods_SocialProject_vers.c
@@ -0,0 +1,1 @@
+ const unsigned char Pods_SocialProjectVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_SocialProject  PROJECT:Pods-1" "\n"; const double Pods_SocialProjectVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ DerivedData/SocialProject/Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-SocialProject.build/Objects-normal/x86_64/Pods_SocialProject_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/markwong/Desktop/WorkExamples/SocialProject/DerivedData/SocialProject/Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-SocialProject.build/DerivedSources/Pods_SocialProject_vers.c
